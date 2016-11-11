2e153cbfa94c5cb5f0c6e116a6f5f4d24ab1304b
ChienHo <467360687@qq.com>
2016-11-11 16:16:42 +0800

    Initial Commit

---
+++ StorageManage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StorageManage
+//
+//  Created by 何成健 on 16/11/11.
+//  Copyright © 2016年 何成健. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StorageManage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StorageManage
+//
+//  Created by 何成健 on 16/11/11.
+//  Copyright © 2016年 何成健. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
